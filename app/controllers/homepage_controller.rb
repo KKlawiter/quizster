13998c3cc1507066a30f11f5204636daee18f899
KKlawiter <katrinaklawiter@gmail.com>
2020-01-08 22:38:23 +0000

    working on errors

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,22 @@
 class HomepageController < ApplicationController
 
 	def index
-		@song = Song.first
-  	end
+		@song = Song.order("RANDOM()").first
+  end
+
+  def new 
+  	@song = Song.new
+  end
+
+
+  def create
+    Song.create(song_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:name, :artist, :genre)
+  end
 end
